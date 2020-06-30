97d864df76ecb8438ffdc34126b7625a5e71b32b
jnnkB <29376715+jnnkB@users.noreply.github.com>
2020-06-30 19:24:11 +0200

    added commandline arguments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import mido
 from pynput.keyboard import Key, Controller
+import argparse
 
-USE_SPACE = False
+parser = argparse.ArgumentParser(description="Push-to-talk using a midi sustain pedal.")
+parser.add_argument("--use-space", dest="use_space", action="store_true")
+parser.add_argument("--use-keyboard-shortcut", dest="use_space", action="store_false")
+parser.set_defaults(use_space=True)
+args = parser.parse_args()
 
 keyboard = Controller()
 
@@ -21,14 +26,14 @@ with mido.open_input("USB MIDI cable Port 2") as inport:
         if msg.type == "control_change" and msg.control == 64:
             if msg.value > 64:
                 if not pressed:
-                    if USE_SPACE:
+                    if args.use_space:
                         keyboard.press(Key.space)
                     else:
                         toggle_shortcut()
                     pressed = True
             else:
                 if pressed:
-                    if USE_SPACE:
+                    if args.use_space:
                         keyboard.release(Key.space)
                     else:
                         toggle_shortcut()
